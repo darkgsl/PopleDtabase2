269449af143bfe567cb661af10d01c9064c31e3b
darkgsl <darkgsl@gmail.com>
2020-02-29 10:23:25 +0300

    Initial Commit

---
+++ PopleDtabase2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PopleDtabase2
+//
+//  Created by Sergey Gomolko on 29/2/20.
+//  Copyright © 2020 Sergey Gomolko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
